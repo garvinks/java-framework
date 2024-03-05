a5999ad5914c3e047cf5ac97b91483eaee94fa83
garvinks <144575241+garvinks@users.noreply.github.com>
2024-03-05 16:07:46 +0800

    feat: init WebBizServer project

---
+++ web-biz-server/src/main/java/com/example/webbizserver/WebBizServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBizServerApplication.class, args);
+	}
+
+}
---
+++ web-biz-server/src/test/java/com/example/webbizserver/WebBizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbizserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
